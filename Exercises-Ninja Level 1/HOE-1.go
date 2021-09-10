aadde1db6af9a027e2f7e01c4636cdce301378a7
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-11 01:55:00 +0530

    Add files via upload

---
+++ Exercises-Ninja Level 1/HOE-1.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main() {
+	x:=42
+	y:="James Bond"
+	z:=true
+	// Printing all the values in a single print statement
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
