d2b86516c07d90635ba30518e32d50ec9f735856
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-12 00:03:39 +0530

    Add files via upload

---
+++ Exercises-Ninja Level 2/HOE-2.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+var a int = 72
+var b int = 72
+var c int = 27
+func main () {
+	fmt.Println(a==b)
+	fmt.Println(a>=b)
+	fmt.Println(a<c)
+	fmt.Println(b>c)
+	fmt.Println(b!=c)
+	fmt.Println(b<=c)
+
+}
