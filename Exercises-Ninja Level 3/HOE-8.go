d5e2fdc06fc803d18b4f4c19d211c207cd6364a1
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-12 23:32:25 +0530

    Add files via upload

---
+++ Exercises-Ninja Level 3/HOE-8.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+	switch {
+	case (1==1):
+		fmt.Println("Don't live in a life full of lie")
+
+
+	case (1==2):
+		fmt.Println("I am not a fan of anyone")
+
+	default:
+		fmt.Println("I am a God")
+
+	}
+}
