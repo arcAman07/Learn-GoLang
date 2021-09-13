d07ad85f1d55011b3bb34e6bb9af793a13783d06
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-13 19:14:27 +0530

    Add files via upload

---
+++ Exercises-Ninja Level 4/HOE-8.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main() {
+	x:=map[string][]string{
+		`bond_james`: {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`:{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	for i,v:=range x{
+		fmt.Printf("The things %v likes to do",i)
+		fmt.Println("")
+		for a,b:=range v{
+			fmt.Printf("\t")
+			fmt.Println(a,b)
+		}
+	}
+
+}
