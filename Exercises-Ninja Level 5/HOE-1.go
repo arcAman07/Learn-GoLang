a764114766981b7aff0339864f8a7059b77ebc96
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-15 18:11:55 +0530

    Add files via upload

---
+++ Exercises-Ninja Level 5/HOE-1.go
@@ -0,0 +1,36 @@
+package main
+import "fmt"
+type person struct {
+	first string
+	last string
+	iceCream []string
+}
+func main() {
+	x:=person{
+		first:"Aman",
+		last:"Sharma",
+		iceCream:[]string{"chocolate","vanilla","blueberry"},
+	}
+	y:=person{
+		first:"Deepak",
+		last:"Sharma",
+		iceCream:[]string{"blackberry","mango","coconut"},
+	}
+	fmt.Printf("The favourite Ice-Cream flavours of %v %v are ",x.first,x.last)
+	fmt.Println(" ")
+
+	for i,v:=range x.iceCream {
+		fmt.Printf("%v)%v",i+1,v)
+		fmt.Println(" ")
+
+	}
+	fmt.Printf("The favourite Ice-Cream flavours of %v %v are ",x.first,x.last)
+	fmt.Println(" ")
+	for a,b:=range y.iceCream {
+		fmt.Printf("%v)%v",a+1,b)
+		fmt.Println(" ")
+
+	}
+
+
+}
