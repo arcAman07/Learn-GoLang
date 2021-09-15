55b35c7acda8d642c6c4841c759083f07a9ef227
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-15 23:27:03 +0530

    Update withPointer.go

--- Reference_Codes/PointersApplied/withPointer.go
+++ Reference_Codes/PointersApplied/withPointer.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	x := 0
+	fmt.Println("x befor", &x)
+	fmt.Println("x befor", x)
+	foo(&x)
+	fmt.Println("x after", &x)
+	fmt.Println("x after", x)
+}
+
+func foo(y *int) {
+	fmt.Println("y befor", y)
+	fmt.Println("y befor", *y)
+	*y = 43
+	fmt.Println("y after", y)
+	fmt.Println("y after", *y)
+}
