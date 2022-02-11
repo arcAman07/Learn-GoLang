1969025e594010c4074ae8b4e5f6a249383369de
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-02-11 19:04:57 +0530

    Add files via upload

---
+++ Reference_Codes/waitGroup.go
@@ -0,0 +1,32 @@
+package main
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+var wg sync.WaitGroup
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("OS:", runtime.GOOS)
+	fmt.Println("Arch:", runtime.GOARCH)
+	wg.Add(1)
+	go foo()
+	bar()
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	wg.Wait()
+}
+
+func foo() {
+	for i:=0; i<10; i++ {
+		fmt.Println("foo:", i)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i:=0; i<10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
